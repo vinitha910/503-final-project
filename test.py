8719b6ea9710c90e77ce81f9762a1039b0d4dc99
Vinitha Ranganeni <vinitha.ranganeni@gmail.com>
2020-02-28 02:07:43 -0800

    simpler test scenario

--- test.py
+++ test.py
@@ -2,27 +2,32 @@ from planner import AStar
 from environment import Environment 
 import numpy as np
 import time 
+from math import pi
+from visualize import visualize 
+from state_space import StateSpace
 
 if __name__ == "__main__":
 	# Input environment length, width and grid resolution (meters)
-	env = Environment(40, 40, 0.05)
+	env = Environment(1, 1)
+	
+	state_space = StateSpace(0.1, 8)
 
 	# Input obstacle length (m), width (m), x (m), y (m), theta (radians)
 	# Make sure all input values are floats
-	o1 = env.create_obstacle(30.0, 10.0, 20.0, 20.0, 0.0)
-	o2 = env.create_obstacle(10.0, 20.0, 10.0, 30.0, 0.0)
-	o3 = env.create_obstacle(10.0, 20.0, 10.0, 30.0, 0.0)
-
-	planner = AStar(env)
-
+	# o1 = env.create_obstacle(2.0, 1.0, 2.0, 2.0, pi/4)
+	# o2 = env.create_obstacle(2, 0.5, 2.0, 3.0, 0.0)
+	planner = AStar(env, state_space)
+	
 	# Input x (m), y (m), theta (radians)
-	planner.set_start(0.5, 0.5, 0.0)
-	planner.set_goal(35.0, 35.0, 0.0)
+	planner.set_start(0.1, 0.1, 0.0)
+	planner.set_goal(0.2, 0.2, 0.0)
+	
+	# visualize(env, [])
 
 	# Planner return whether or not it was successful, 
 	# the number of expansions, and time taken 
 	success, num_expansions, time = planner.plan()
-
+	
 	# If planner was successful, extract the path
 	if success:
 		path = planner.extract_path()
