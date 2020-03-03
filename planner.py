Manually merge overflow bug; make bugs globally configurable

planner.py
@@ -4,6 +4,8 @@ from min_binary_heap import MinBinaryHeap
 from environment import Environment 
 from math import pi 
 import time, sys
+import numpy as np
+from bug_config import *
 
 TIMEOUT = 60.0
 
@@ -25,7 +27,7 @@ class AStar():
             print("[Planner] Invalid start state")
             return False
         state = self.state_space.get_or_create_state(x, y, theta)
-        self.start = Node(0, -1, state.id)
+        self.start = Node(np.int32(0), -1, state.id)
         return True
 
     # TO DO: Create goal region for non-point robots
@@ -36,7 +38,7 @@ class AStar():
             print("[Planner] Invalid goal state")
             return False
         self.goal_state = self.state_space.get_or_create_state(x, y, theta)
-        self.goal = Node(0, -1, self.goal_state.id)
+        self.goal = Node(np.int32(0), -1, self.goal_state.id)
         return True
             
     def is_goal(self, state):
@@ -92,7 +94,10 @@ class AStar():
                 # Scale g-value so that h is never greater than g
                 # If h > g we will produce a sub-optimal path because it's as if
                 # we are scaling h by some epsilon
-                alt_f = 1000*alt_g + h
+                if BUG_NO[0] == BUGNO_OVERFLOW:
+                    alt_f = np.int32(1000*alt_g + h)
+                else:
+                    alt_f = 1000*alt_g + h
 
                 # If the successor has not been visited OR the node is in the 
                 # open list AND alt_g < the previously calculated g

state_space.py
@@ -3,6 +3,7 @@
 from math import floor, pi
 import numpy as np
 from robots.robot import Robot 
+from bug_config import *
 
 class State(object):
     def __init__(self, state_id, x, y, theta):
@@ -95,8 +96,12 @@ class StateSpace(object):
         x2, y2, th2 = self.discrete_coor_to_continuous(
             state_2.x, state_2.y, state_2.theta)
 
-        return np.linalg.norm(
-            [x1 - x2, y1 - y2, th1 - th2]) 
+        if BUG_NO[0] == BUGNO_OVERFLOW:
+            return np.int32(2**18.1*1000*np.linalg.norm(
+                [x1 - x2, y1 - y2, th1 - th2]))
+        else:
+            return np.linalg.norm(
+                [x1 - x2, y1 - y2, th1 - th2])
 
     def in_collision(self, x, y, theta):
         x_m, y_m, theta_rad = \