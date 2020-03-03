cbcd5bf05e5216a41d014b00f9682ad6536eb1f0
Nathan Hatch <nathanhatch297@gmail.com>
2020-03-03 11:39:40 -0800

    Manually merge changes from opt_start_goal

--- validate.py
+++ validate.py
@@ -1,11 +1,11 @@
 from planner import AStar, TIMEOUT
-from environment import Environment 
+from environment import Environment
 import numpy as np
-import time 
+import time
 from math import pi
-from visualize import Visualizer 
+from visualize import Visualizer
 from state_space import StateSpace
-from robots.rectangle_robot import RectangleRobot 
+from robots.rectangle_robot import RectangleRobot
 from robots.circle_robot import CircleRobot
 from cma import CMA
 import sys
@@ -18,12 +18,16 @@ data_file = open(path, "w")
 csv_writer = csv.writer(data_file, delimiter=',')
 img_path = "progress-"+run_id+".png"
 
+NUM_OBSTACLES = 2
+OBSTACLE_DIM = 4
+M = NUM_OBSTACLES*OBSTACLE_DIM
+
 def clamp_obs(obs):
     return (obs-(obs-1)*(obs<1)).astype(np.int)
 
 def run_planner(env_parameters, render=None):
         resolution_m = 0.01
-        params = clamp_obs(env_parameters)
+        obs_params = clamp_obs(env_parameters[:M])
 
         # Statespace can take a PointRobot, SquareRobot, RectangleRobot objects
         # robot = PointRobot(0, 0)
@@ -34,7 +38,7 @@ def run_planner(env_parameters, render=None):
         # Takes discrete values, divide continuous values by resolution
         # Parameters: environment length, width, 2D array with obstacle parameters
         # e.g. [[l1, w1, x1, x2], [l2, w2, x2, y2],..., [ln, wn, xn, yn]]
-        env = Environment(100, 100, [params[:4], params[4:8]])
+        env = Environment(100, 100, [obs_params[:4], obs_params[4:8]])
 
         # Parameters: resolution (m), number of theta values, robot object,
         # and environment object
@@ -46,9 +50,10 @@ def run_planner(env_parameters, render=None):
         success, num_expansions, planning_time = True, 0, 0.0
 
         # Input x (m), y (m)
-        if not (planner.set_start(0.4, 0.7, pi/4)):
+        if not (planner.set_start(env_parameters[-4]/100., env_parameters[-3]/100., pi/4)):
+            print(env_parameters[-4], ' ' , env_parameters[-5])
             success = False # no expansions, since initial config was invalid
-        if not (planner.set_goal(0.7, 0.8, pi/4)):
+        if not (planner.set_goal(env_parameters[-2]/100., env_parameters[-1]/100., pi/4)):
             success = False # ditto
 
         # Planner return whether or not it was successful,
@@ -82,13 +87,14 @@ def run_planner(env_parameters, render=None):
             ])
 
         if error:
-            print("Your planner is buggy! Check out this failing test:", ", ".join(params.astype(str)))
+            print("Your planner is buggy! Check out this failing test:", ", ".join(obs_params.astype(str)),
+                ", ".join(env_parameters[M:]))
             sys.exit(0)
 
         return -num_expansions
 
 if __name__ == "__main__":
-    initial_mean = np.array([20, 5, 57, 58, 5, 5, 44, 85])
+    initial_mean = np.array([20, 5, 57, 58, 5, 5, 44, 85, 40, 70, 70, 80])
     #initial_mean = np.array([59,1,59,60,1,24,38,110])
     initial_sigma = 2.0
     initial_cov = np.eye(len(initial_mean))
