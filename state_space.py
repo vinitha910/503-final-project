Make heuristic overflow bug slightly harder to find

state_space.py
@@ -97,7 +97,7 @@ class StateSpace(object):
             state_2.x, state_2.y, state_2.theta)
 
         if BUG_NO[0] == BUG_OVERFLOW:
-            return np.int32(2**18.1*1000*np.linalg.norm(
+            return np.int32(2**18.1*600*np.linalg.norm(
                 [x1 - x2, y1 - y2, th1 - th2]))
         
         return np.linalg.norm(